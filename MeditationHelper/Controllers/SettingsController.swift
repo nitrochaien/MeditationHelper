f746e6a4195f7c5cb0f3e6d5d29d4fd0dafb2874
Dinh Vu Nam <namdinh@hiworld.com.vn>
2017-08-10 10:58:37 +0700

    init settings controller

---
+++ MeditationHelper/Controllers/SettingsController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsController.swift
+//  MeditationHelper
+//
+//  Created by Dinh Vu Nam on 8/10/17.
+//  Copyright © 2017 NamDV. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
